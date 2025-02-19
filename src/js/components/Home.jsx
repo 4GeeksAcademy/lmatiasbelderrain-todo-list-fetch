f033deb0557cc9d3aa8c651f10918727edd75593
lmatiasbelderrain <Matibelde@hotmail.com>
2025-02-19 18:44:35 +0000

    obtener tareas

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [lista,setLista]=useState([]) //la lista de tareas comienza como un array vacio
+	const [tarea,setTarea]=useState("")
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const obtenerTareas=async()=>{
+		try {
+			const response = await fetch("https://playground.4geeks.com/todo/users/mati_belde")
+			console.log(response)
+			const data = await response.json()
+			console.log(data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	
+const agregarTarea=(e)=>{
+	if (tarea==""){
+		alert("No se ingreso tarea")
+	}else {
+		setLista([...lista,tarea])
+setTarea("")
+	}
+}    
+
+const eliminarTarea = (indice)=>{
+	if (indice >= 0 && indice < lista.length) {
+        const nuevaLista = lista.filter((item, i) => i !== indice);  
+        setLista(nuevaLista);
+    } else {
+        alert("Índice no válido");
+    }
+	
+}
+useEffect(()=>{
+obtenerTareas()
+},[]) // Se ejecuta una sola vez
+
+	return (
+		<div className="text-center container">
+			<h1>To Do List</h1>
+			<div className="d-flex justify-content-center m-3">
+
+
+				<input
+					className="form-control"
+					type="text"
+					value={tarea}
+					onChange={(e)=>setTarea(e.target.value)}
+				/>
+				<button
+					className="btn btn-success"
+					onClick={(e)=>agregarTarea(e)}
+				>
+					Agregar Tarea
+				</button>
+			</div>
+			<ul className="list-group">
+				{lista.map((item,id)=>(
+					<li className="list-group-item" key={id}>
+						{item}
+						<button className="btn btn-outline-danger float-end icono"
+						
+						onClick={()=>eliminarTarea(id)}
+						>X</button>
+						</li>
+				))}
+			</ul>
+			<h4 className="mt-2">Tareas Pendientes: {lista.length}</h4>
 		</div>
+
 	);
 };
 
